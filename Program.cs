f29d04dbbba394b87d7233090bee57091292a70d
Toni Gregov <tonigregov@outlook.com>
2021-10-17 14:22:37 +0200

    Added cache busting

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -13,13 +14,22 @@ namespace CloudSurf
 {
     public class Program
     {
+        protected static string ServerLocation { get; } = "https://localhost:44313/";
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddTransient(sp => new HttpClient(new DefaultBrowserOptionsMessageHandler(new HttpClientHandler()) // new HttpClientHandler() in .NET 5.0
+            {
+                DefaultBrowserRequestCache = BrowserRequestCache.NoStore,
+                DefaultBrowserRequestCredentials = BrowserRequestCredentials.Include,
+                DefaultBrowserRequestMode = BrowserRequestMode.Cors,
+            })
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
+            });
             await builder.Build().RunAsync();
         }
     }
